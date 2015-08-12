67b155f56d67cd3544ca0d54707a611455d5eb6d
smachefert <smachefert@vps172586.ovh.net>
2015-08-12 15:10:12 +0200

    script to process HistoricalText files

---
+++ HistoricalTexts/process_HT_files.py
@@ -0,0 +1,34 @@
+﻿#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from pymarc import MARCReader, marc8_to_unicode
+import os, urllib
+
+prefix = "https://journalarchives.jisc.ac.uk/Shibboleth.sso/Login?entityID=https%3A%2F%2Fidp.yorksj.ac.uk%2Foala&target="
+
+for subdir, dirs, files in os.walk("."):
+	if subdir == "./OUT": continue
+	for f in [fi for fi in files if fi.endswith(".mrc")]:
+		filename = os.path.join(subdir,f)
+		print "# # # # # # # # # # # #"
+		print "Reading: %s" % filename
+		reader = MARCReader(open("%s" % filename), force_utf8=True);
+		
+		outputFilename = "OUT/%s" % os.path.basename(filename)
+		print "Output:  %s" % outputFilename
+		out = open(outputFilename, 'wb');
+		nb = 0
+		for record in reader:
+			nb += 1
+
+			for f856 in record.get_fields("856"):
+				if len(f856.get_subfields('u')) != 1:
+					print "Wrong number of 856u"
+					exit()
+				else:
+					url = "%s%s" % (prefix, urllib.quote_plus(f856.get_subfields('u')[0]))
+					f856.delete_subfield("u")
+					f856.add_subfield("u", url)
+					f856.add_subfield("z", "Click here to access electronic holdings.")
+			out.write(record.as_marc())
+		print "%s records in file" % nb
+		print ""

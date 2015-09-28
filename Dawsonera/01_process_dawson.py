56424912bcbe394c2f2dcb34b6df0038bd03f8f0
smachefert <smachefert@vps172586.ovh.net>
2015-09-28 17:00:25 +0200

    Adding the first version of scripts for Dawsonera

---
+++ Dawsonera/01_process_dawson.py
@@ -0,0 +1,101 @@
+﻿#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from pymarc import MARCReader, marc8_to_unicode, record
+import os, urllib
+
+nb_total = 0
+nb_total_new = 0
+
+'''
+This file will process the Dawsonera files for the following enhancements:
+- Remove any 020 that is not for e-book
+'''
+f001done = {}
+outputFilename = "TMP/global-step1.mrc"
+out = open(outputFilename, 'wb');
+for subdir, dirs, files in os.walk("SRC/TMP"):
+	for f in sorted([fi for fi in files if fi.endswith("USM")], reverse=True):
+		filename = os.path.join(subdir,f)
+		print "# # # # # # # # # # # #"
+		print "Reading: %s" % filename
+		reader = MARCReader(open("%s" % filename))
+
+		nb = 0
+		nb_new = 0
+		for record in reader:
+			f001 = record.get_fields("001")[0].data
+
+			if f001 in f001done:
+				#print "Skipping %s" % f001
+				pass
+			else:
+				f001done[f001] = f001
+				nb_isbn_ebook = 0
+				nb += 1					
+				for f010 in record.get_fields("010"):
+					record.remove_field(f010)
+				
+				for f015 in record.get_fields("015"):
+					record.remove_field(f015)
+				
+				for f016 in record.get_fields("016"):
+					record.remove_field(f016)
+				
+				for f020 in record.get_fields("020"):
+					isbn_ebook = ""
+					f020_out = []
+					for i in range(0, len(f020.subfields), 2): 
+						code = f020.subfields[i]
+						value = f020.subfields[i + 1]
+
+						if "e-book" in value:
+							nb_isbn_ebook += 1
+							if isbn_ebook != "":
+								print "###################################"
+								print "###################################"
+								print "######## ", isbn, " duplicated ########"
+								print "###################################"
+								print "###################################"
+								exit()
+							isbn_ebook = value
+							f020_out.append("a")
+							f020_out.append(value)
+					
+					if isbn_ebook == "":
+						record.remove_field(f020)
+					else:
+						f020.subfields = f020_out
+				
+				# We check the 856 because at the moment they don't contain $z
+				nb856 = 0
+				for f856 in record.get_fields("856"):
+					tab_z = f856.get_subfields("z")
+					nb856 += 1
+					if len(tab_z) == 0:
+						f856.add_subfield("z", u"View eBook online.")
+				
+				if nb856 == 0:
+					print "No 856 for this"
+					exit()
+				
+				if len(record.get_fields("020")) == 0:
+					print "Pas de 020 pour %s !" % f001
+				elif nb_isbn_ebook == 0:
+					print "020 but no e-book one for %s" % f001
+				try:
+					out.write(record.as_marc())
+				except:
+					record.set_force_utf8(True)
+					try:
+						out.write(record.as_marc())
+					except:
+						print "Unable to export %s" % record.get_fields("001")[0].data
+						pass
+				
+		nb_total += nb
+
+		print "%s records in file" % (nb)
+out.close()
+
+
+print "Total records: %s # Unique records: %s" % (nb_total, nb_total_new)
---
+++ Dawsonera/02_verifExists.py
@@ -0,0 +1,109 @@
+﻿#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from pymarc import MARCReader, marc8_to_unicode
+import os, urllib, re, json, sys
+
+nb_total = 0
+nb_total_new = 0
+filename = "global-step1.mrc"
+reader = MARCReader(open("TMP/%s" % filename))
+
+outfile = open("OUT/dawsonera-20150922-ok-import.mrc", "w")
+outfile_exception = open("OUT/dawsonera-20150922-manually-handle.txt", "w")
+outfile_exception_noisbn = open("OUT/dawsonera-20150922-noisbn.mrc", "w")
+
+def writeRecord(record, fh):
+	try:
+		fh.write(record.as_marc())
+	except:
+		record.set_force_utf8(True)
+		try:
+			fh.write(record.as_marc())
+		except:
+			e = sys.exc_info()[0]
+			print "Unable to export %s" % record.get_fields("001")[0].data
+			print e
+			exit()
+			pass
+
+nb = 0
+nb_new = 0
+nb_keep = 0
+for record in reader:
+	nb += 1
+	if nb > 5000:
+		exit()
+	else:
+		keepRecord = True
+		noIsbn = False
+		f001 = record.get_fields("001")[0].data
+		
+		if len(record.get_fields("020")) > 0:
+			isbn = str(record.get_fields("020")[0].value().encode("utf-8")).replace(" (e-book)", "")
+			isbn = isbn.upper()
+			if not re.match(r"^[\dXx]+$", isbn):
+				for f020 in record.get_fields("020"):
+					print f020.value().encode("utf-8")
+				print "Error with ISBN #%s# [%s]" % (isbn, f001)
+			
+			url = "http://capitadiscovery.co.uk/yorksj/items.json?query=isbn%3A" + isbn + "&target=catalogue"
+			response = urllib.urlopen(url)
+			data = json.loads(response.read())
+			
+			# We can find at least page with this ISBN
+			if len(data) > 0:
+			
+				bibid = ""
+				hasUrl = False
+				hasUrlFull = ""
+				
+				for a in data:
+					if re.match(r"http:\/\/capitadiscovery\.co\.uk\/yorksj\/items\/(.*)$", a):
+						url = a
+						bibid = re.sub(r"http:\/\/capitadiscovery\.co\.uk\/yorksj\/items\/(.*)$", r"\1", url)
+						# We are going to load the json of the record
+						responseDetailed = urllib.urlopen(url + ".json")
+						dataDetailed = json.loads(responseDetailed.read())
+						for b in dataDetailed:
+							if b == url:
+								for c in dataDetailed[b]:
+									if c == "http://purl.org/dc/terms/hasVersion":
+										hasUrl = True
+										if len(dataDetailed[b][c]) == 1:
+											hasUrlFull = dataDetailed[b][c][0]["value"]
+										else:
+											print "TOO MUCH SIZE"
+											exit()
+				
+				if hasUrl:
+					if "dawsonera" in hasUrlFull:
+						keepRecord = False
+						print "EXISTS with URL on dawsonera %s [%s]" % (f001, bibid)
+					else:
+						print "EXISTS with URL outside dawsonera %s [%s]" % (f001, bibid)
+						keepRecord = False
+			else:
+				print "# NEW: %s - %s" % (isbn, record["245"].get_subfields("a")[0])
+				keepRecord = False
+		else:
+			# No ISBN, how will this match?
+			keepRecord = False
+			noIsbn = True
+
+		
+		if keepRecord:
+			nb_keep += 1
+			writeRecord(record, outfile)
+		else:
+			if noIsbn:
+				writeRecord(record, outfile_exception_noisbn)
+			else:
+				outfile_exception.write("##########\n")
+				for field in record:
+					fieldValue = field.__str__().encode("utf-8")
+					outfile_exception.write(fieldValue + "\n")
+				outfile_exception.write("\n\n")
+
+
+outfile.close()
+print "Out of %s, %s will be updated" % (nb, nb_keep)

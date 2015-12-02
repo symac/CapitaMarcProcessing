7acf295786bf4ea31bed5433747c87ebdfd0df65
smachefert <smachefert@vps172586.ovh.net>
2015-12-02 18:46:35 +0100

    First version of the ebsco scripts

---
+++ ebsco/01_process_ebsco.py
@@ -0,0 +1,86 @@
+﻿#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from pymarc import MARCReader, marc8_to_unicode
+import os, urllib
+from os import listdir
+from os.path import isfile, join
+import sys
+
+nb_total = 0
+nb_total_new = 0
+
+prefix = "http://ezproxy.yorksj.ac.uk/login?url=";
+
+class ExceptionHandlingIterator(object):
+	def __init__(self, iterable):
+		self._iter = iter(iterable)
+		self.handlers = []
+	
+	def __iter__(self):
+		return self
+
+	def next(self):
+		global nb
+		try:
+			return self._iter.next()
+		except StopIteration as e:
+			raise e
+		except ValueError as e:
+			return self.next()
+		except Exception as e:
+			print "####"
+			print type(e)
+			return self.next()
+
+# We need to clean the TMP file first
+tmpFiles = [ f for f in listdir("TMP") if isfile(join("TMP",f)) ]
+for f in tmpFiles:
+	os.remove(join("TMP",f))
+	print "Cleaning TMP: %s" % f
+			
+'''
+This file will process the askews files to remove any 020 that is not for an ebook
+'''
+f001done = {}
+for subdir, dirs, files in os.walk("SRC"):
+	for f in sorted([fi for fi in files if fi.endswith(".mrc") or fi.endswith(".bin")], reverse=True):
+		print f
+		filename = os.path.join(subdir,f)
+		print "# # # # # # # # # # # #"
+		print "Reading: %s" % filename
+
+		fh = open(filename, 'rb')
+		reader = ExceptionHandlingIterator(MARCReader(fh, force_utf8=True, utf8_handling='ignore', to_unicode=True))
+		
+		outputFilename = "TMP/%s-clean.mrc" % os.path.splitext(f)[0]
+		out = open(outputFilename, 'wb');
+		nb = 0
+		nb_new = 0
+		for record in reader:
+			nb += 1
+			f001 = record.get_fields("001")[0].data
+			if f001 in f001done:
+				# print "Skipping %s, already done in %s" % (f001, f001done[f001])
+				pass
+			else:
+				nb_new += 1
+				f001done[f001] = f
+				
+				for f856 in record.get_fields("856"):
+					if len(f856.get_subfields('u')) != 1:
+						print "Wrong number of 856u"
+					else:
+						url = "%s%s" % (prefix, f856.get_subfields('u')[0])
+						f856.delete_subfield("u")
+						f856.add_subfield("u", url)
+						f856.add_subfield("z", "View eBook online.")
+						out.write(record.as_marc())
+		
+		nb_total += nb
+		nb_total_new += nb_new
+		print "%s records in file [%s new]" % (nb, nb_new)
+		out.close()
+
+print "\n\nTotal records: %s # Unique records: %s" % (nb_total, nb_total_new)
+os.chdir("TMP")
+os.system("mergeFiles.py -o mergedFile.mrc")
---
+++ ebsco/02_verifExists.py
@@ -0,0 +1,94 @@
+﻿#!usr/bin/env python
+# -*- coding: utf-8 -*-
+from pymarc import MARCReader, marc8_to_unicode
+import os, urllib, re, json
+from datetime import date
+import sys
+nb_total = 0
+nb_total_new = 0
+filename = "mergedFile.mrc"
+reader = MARCReader(open("TMP/%s" % filename), force_utf8=True);
+
+today =  date.today()
+
+outfile = open("OUT/mergedFile-loading-ok-%s.mrc" % today, "w")
+outfile_exception = open("OUT/mergedFile-manually-handle-%s.mrc" % today, "w")
+
+nb = 0
+nb_new = 0
+nb_keep = 0
+for record in reader:
+	nb += 1
+	f001 = record.get_fields("001")[0].data
+	isbn = str(record.get_fields("020")[0].value())
+	isbn = isbn.replace(" (e-book) :", "")
+	isbn = isbn.replace(" (electronic bk.)", "")
+	isbn = isbn.replace(" electronic bk.", "")
+	isbn = isbn.replace(" (e-book)", "")
+
+	keepRecord = True
+	
+	if not re.match(r"^[\dX]+$", isbn):
+		print record.get_fields("020")[0].value()
+		print "Error with ISBN #%s# [%s]" % (isbn, f001)
+		sys.exit()
+	
+	url = "http://capitadiscovery.co.uk/yorksj/items.json?query=isbn%3A" + isbn + "&target=catalogue"
+	url = "http://capitadiscovery.co.uk/yorksj/items.json?facet%5B0%5D=format%3A%22ebook%22&query=isbn%3A" + isbn + "&adv=t"
+	response = urllib.urlopen(url)
+	content = response.read()
+	try:
+		data = json.loads(content)
+	except:
+		print "UNABLE TO READ (no result) %s" % isbn
+		data = [];
+
+	# We can find at least page with this ISBN
+	if len(data) > 0:
+		bibid = ""
+		hasUrl = False
+		hasUrlFull = ""
+		
+		for a in data:
+			if re.match(r"http:\/\/capitadiscovery\.co\.uk\/yorksj\/items\/(.*)$", a):
+				url = a
+				bibid = re.sub(r"http:\/\/capitadiscovery\.co\.uk\/yorksj\/items\/(.*)$", r"\1", url)
+				# We are going to load the json of the record
+				responseDetailed = urllib.urlopen(url + ".json")
+				dataDetailed = json.loads(responseDetailed.read())
+				for b in dataDetailed:
+					if b == url:
+						for c in dataDetailed[b]:
+							if c == "http://purl.org/dc/terms/hasVersion":
+								hasUrl = True
+								if len(dataDetailed[b][c]) == 1:
+									hasUrlFull = dataDetailed[b][c][0]["value"]
+								else:
+									print "TOO MUCH SIZE"
+									exit()
+		
+		if hasUrl and bibid:
+			if "ebscohost" in hasUrlFull:
+				print "EXISTS with URL on ebscohost %s [%s], will be excluded" % (f001, bibid)
+				keepRecord = False
+			else:
+				print "EXISTS with URL outside vlebooks %s [%s]" % (f001, bibid)
+				print hasUrlFull
+				exit()
+		else:
+			# We need to control them. Best way would be 
+			print "####### WARNING " + isbn + " (print only?)"
+	else:
+		print "# NEW: %s - %s" % (isbn, record["245"].get_subfields("a")[0])
+		keepRecord = True
+		
+	if keepRecord:
+		nb_keep += 1
+		outfile.write(record.as_marc())
+	else:
+		outfile_exception.write(str(record))
+		outfile_exception.write("\n")
+
+
+outfile.close()
+print "Out of %s, %s will be updated" % (nb, nb_keep)
